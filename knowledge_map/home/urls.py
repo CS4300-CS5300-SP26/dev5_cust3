669596634f3ed212c1dc121e5dd67e6814b67249
trinityjfalcon <trinity.falcon1@gmail.com>
2026-03-09 17:51:58 +0000

    update urls and create templates and extending base for navigation bar

--- knowledge_map/home/urls.py
+++ knowledge_map/home/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('home/', views.home, name='home'),
+    path('new_map/', views.create_map, name='create_map'),
+    path('quiz/', views.quiz, name='quiz'),
+    path('progress/', views.progress, name='progress'),
+    path('logout/', views.logout_view, name='logout'),
 ]
