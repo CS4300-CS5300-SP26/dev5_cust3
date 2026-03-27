8764272f02db7940d7587e3313128e0487410e17
Abraham Anderson <abeoanderson@gmail.com>
2026-03-27 13:22:05 -0600

    Refactor setUp methods for upload and delete tests
    Refactor setUp methods in UploadPageTest and DeleteFileTest to ensure user login is handled correctly.

--- knowledge_map/knowledge_app/tests.py
+++ knowledge_map/knowledge_app/tests.py
@@ -98,7 +98,12 @@ class NavbarTest(TestCase):
 
 # ----------------Tests for Upload Feature---------------------
 class UploadPageTest(TestCase):
-
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpass123'
+        )
+        self.client.login(username='testuser', password='testpass123')
+        
     def setUp(self):
         self.client = Client()
 
@@ -149,10 +154,14 @@ class UploadPageTest(TestCase):
         for f in UploadedFile.objects.all():
             if os.path.exists(f.file.path):
                 os.remove(f.file.path)
-
 # ----------------Tests for Delete Feature---------------------
 class DeleteFileTest(TestCase):
-
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpass123'
+        )
+        self.client.login(username='testuser', password='testpass123')
+    
     def setUp(self):
         self.client = Client()  # set up fake browser before each test
 
