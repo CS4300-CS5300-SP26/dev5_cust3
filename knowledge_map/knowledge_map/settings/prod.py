9007f6a15d0ee3949d8cbef07c309734636e3211
trinityjfalcon <trinity.falcon1@gmail.com>
2026-04-07 01:46:13 +0000

    adding if else for database

--- knowledge_map/knowledge_map/settings/prod.py
+++ knowledge_map/knowledge_map/settings/prod.py
@@ -14,16 +14,24 @@ ALLOWED_HOSTS = ['yourknowledgemap.me', '157.230.89.215']
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 #for when we use a database in deployment, for now we will just use sqlite
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+if all([os.getenv("DB_NAME"), os.getenv("DB_USER"), os.getenv("DB_PASSWORD"), os.getenv("DB_HOST")]):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv("DB_NAME"),
+            'USER': os.getenv("DB_USER"),
+            'PASSWORD': os.getenv("DB_PASSWORD"),
+            'HOST': os.getenv("DB_HOST"),
+            'PORT': os.getenv("DB_PORT"),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 #sqlite db for the testing
 #DATABASES = {
