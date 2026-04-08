6c87c5bcfebdde217554dfe46e32e6e81abe4086
Nathan Schluessler <nathan@keymaker.info>
2026-04-08 04:34:10 +0000

    More configuration changes

--- knowledge_map/knowledge_map/settings/base.py
+++ knowledge_map/knowledge_map/settings/base.py
@@ -149,6 +149,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Celery settings
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_CACHE_BACKEND = 'default'
