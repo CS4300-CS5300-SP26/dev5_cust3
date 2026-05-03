ca54cf175268ac92af80a52a95b3649b153c90fb
Nathan Schluessler <nathan@keymaker.info>
2026-05-03 02:05:18 +0000

    Fixing linting error

--- knowledge_map/knowledge_map/urls.py
+++ knowledge_map/knowledge_map/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("knowledge_app.urls")),
     # serves uploaded files via URL
-] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
