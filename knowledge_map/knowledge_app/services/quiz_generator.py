09879bd79c27ed27a6f62fe1dd1a95ef172a9a89
trinityjfalcon <trinity.falcon1@gmail.com>
2026-05-05 05:42:42 +0000

    linting

--- knowledge_map/knowledge_app/services/quiz_generator.py
+++ knowledge_map/knowledge_app/services/quiz_generator.py
@@ -391,9 +391,6 @@ Text to generate questions from:
         # Parse the JSON response into a list of question dictionaries
         questions_data = json.loads(raw)
 
-        # Save each generated question to the database
-        from knowledge_app.models import Question as QuestionModel
-
         for order, q_data in enumerate(questions_data, start=1):
             Question.objects.create(
                 quiz=quiz,
